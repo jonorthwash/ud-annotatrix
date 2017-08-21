85ae29e521ca35e77790fdf364e1db973d017fc7
maryszmary <masha.shejanova@gmail.com>
2017-08-21 23:32:32 +0300

    comments separation

--- standalone/lib/CG2conllu.js
+++ standalone/lib/CG2conllu.js
@@ -6,9 +6,38 @@ function testConversion() {
 }
 
 function CG2conllu(CGtext) {
+	/* Takes a string in CG, returns a string in conllu. */
+	var sent = new conllu.Sentence();
 	console.log(CGtext);
+	var separated = findComments(CGtext);
+	sent.comments = separated[0];
 
-	return "0";
+	var tokens = formTokens(separated[1]);
+	sent.tokens = tokens;
+
+	return sent.serial;
+}
+
+
+function findComments(CGtext) {
+	/* Takes a string in CG, returns 2 arrays with strings. */
+	var lines = CGtext.split("\n");
+	var comments = [];
+	var tokens = [];
+	$.each(lines, function(n, line) {
+		if (line[0] == "#") {
+			comments.push(line);
+		} else {
+			tokens.push(line);
+		}
+	});
+	return [comments, tokens];
+}
+
+
+function formTokens(lines) {
+	var id = 1;
+	return [];
 }
 
 
