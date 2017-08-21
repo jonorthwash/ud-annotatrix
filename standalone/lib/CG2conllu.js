cd8a7d90fe94fe1c285e00a287d5a95c6376bcf1
maryszmary <masha.shejanova@gmail.com>
2017-08-22 00:32:33 +0300

    getting token analyses

--- standalone/lib/CG2conllu.js
+++ standalone/lib/CG2conllu.js
@@ -1,9 +1,4 @@
-function testConversion() {
-    var CG = $("#indata").val();
-    var conStr = CG2conllu(CG);
-    console.log("result: ");
-    console.log(conStr);
-}
+"use strict"
 
 function CG2conllu(CGtext) {
     /* Takes a string in CG, returns a string in conllu. */
@@ -39,20 +34,40 @@ function formTokens(lines) {
     var tokens = [];
     for (var i = 0; i < (lines.length); i += 2) {
         if (lines[i].match(/"<.*>"/)) { // then everything is ok
+            var analyses = {};
             var id = i/2 + 1;
             var form = lines[i].replace(/"<(.*)>"/, '$1');
             console.log(id + ": " + form);
 
-            var attrs = {
-                "id": id,
-                "form": form
-            };
-            tokens.push(formNewToken(attrs));
+            if (lines[i + 1] && !lines[i + 1].match(/"<.*>"/)) { // then everything is ok
+                analyses = getAnalyses(lines[i + 1]); 
+            } else {
+                console.log("Something gone wrong on line: " + lines[i + 1]);
+            }
+
+            analyses.id = id;
+            analyses.form = form;
+            tokens.push(formNewToken(analyses));
         } else {
             console.log("Malformed CG!");
         }
     }
     return tokens;
+}
+
+
+function getAnalyses(line) {
+    var analyses = {}
+
+    // first replace space (0020) with · for lemmas and forms containing
+    // whitespace, so that the parser doesn't get confused.
+    var quoted = line.replace(/.*(".*?").*/, '$1');
+    var forSubst = quoted.replace(/ /g, "·");
+    var gram = line.replace(/".*"/, forSubst);
+
+    gram = gram.split(" "); // then split on space
+
+    return analyses;
 }
 
 
