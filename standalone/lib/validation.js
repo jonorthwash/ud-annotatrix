6c696841694a0936150d1487d97a588b92b295fa
Francis Tyers <ftyers@prompsit.com>
2017-10-13 15:41:10 +0200

    comments

--- standalone/lib/validation.js
+++ standalone/lib/validation.js
@@ -4,8 +4,14 @@ var U_DEPRELS = ["acl", "advcl", "advmod", "amod", "appos", "aux", "case", "cc",
 var U_POS = ["ADJ", "ADP", "ADV", "AUX", "CCONJ", "DET", "INTJ", "NOUN", "NUM", "PART", "PRON", "PROPN", "PUNCT", "SCONJ", "SYM", "VERB", "X"];
 
 function is_udeprel(s) {
+    var s_deprel = s;
+    // Language specific relations are a universal relation + : + some string
+    if(s.search(":") >= 0) {
+      s_deprel = s.split(":")[0];
+    }
+    // Check if the deprel is in the list of valid relations
     for(var i = 0; i < U_DEPRELS.length; i++) {
-      if(U_DEPRELS[i] == s) { 
+      if(U_DEPRELS[i] == s_deprel) { 
         return true;
       }
     }
