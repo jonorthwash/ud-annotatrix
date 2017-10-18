4052933489bc0b3d6d7e0d954e14a2dd45289246
jonorthwash <jonathan.north.washington@gmail.com>
2017-10-18 19:55:11 -0400

    @sushain97's suggestion for alignment issues, pending another approach

--- standalone/lib/gui.js
+++ standalone/lib/gui.js
@@ -768,9 +768,9 @@ $(document).ready(function(){
 
 	$('#helpModal').on('show.bs.modal', console.log);
 
-	$('#helpModal').on('shown.bs.modal', function() {
-		alert('HARGLE BARGLE');
-		//$(e.target).find('.modal-body').load('help.html');
+	$('#helpModal').on('shown.bs.modal', function(e) {
+		//alert('HARGLE BARGLE');
+		$(e.target).find('.modal-body').load('help.html');
 	});
 
 	$('.ui-autocomplete').keydown(function(e) {
