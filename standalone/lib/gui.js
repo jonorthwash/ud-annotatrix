df393cb3c043d5ceadf0f00aa64ee24c02c4c818
Francis Tyers <ftyers@prompsit.com>
2017-10-19 19:53:06 +0200

    aux can be cop

--- standalone/lib/gui.js
+++ standalone/lib/gui.js
@@ -79,17 +79,18 @@ function writeArc(sourceNode, destNode) {
     console.log('writeArc ' + destIndex + ' ' + thisToken['upostag']); 
     var sentAndPrev = changeConlluAttr(sent, indices, "head", sourceIndex);
 
-    // If the target POS tag is PUNCT set the deprel to punct
-    // IF the target POS tag is DET set the deprel to det
-    // IF the target POS tag is AUX set the deprel to aux
+    // If the target POS tag is PUNCT set the deprel to @punct
+    // IF the target POS tag is DET set the deprel to @det
     // TODO: Put this somewhere better
     if(thisToken['upostag'] == 'PUNCT') {
       sentAndPrev = changeConlluAttr(sent, indices, "deprel", "punct");
     }else if(thisToken['upostag'] == 'DET') {
       sentAndPrev = changeConlluAttr(sent, indices, "deprel", "det");
-    }else if(thisToken['upostag'] == 'AUX') {
-      sentAndPrev = changeConlluAttr(sent, indices, "deprel", "aux");
     }
+     
+    // AUX can be @cop also
+//    }else if(thisToken['upostag'] == 'AUX') {
+//      sentAndPrev = changeConlluAttr(sent, indices, "deprel", "aux");
 
 //    var sentAndPrev = changeConlluAttr(sent, indices, "head", sourceIndex);
     sent = sentAndPrev[0];
