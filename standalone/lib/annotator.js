da5d6e31626fd1da410fdbd488b1c58e5f3ea541
jonorthwash <jonathan.north.washington@gmail.com>
2017-10-19 19:50:30 -0400

    upgraded and fixed some js dependencies

--- standalone/lib/annotator.js
+++ standalone/lib/annotator.js
@@ -17,7 +17,7 @@ var LABELS = [];
 function main() {
     head.js(
         ROOT + 'ext/jquery-3.2.1.min.js',
-        ROOT + 'ext/jquery-ui.min.js',
+        ROOT + 'ext/jquery-ui-1.12.1/jquery-ui.min.js',
         ROOT + 'ext/cytoscape.min.js',
         ROOT + 'ext/undomanager.js',
         ROOT + 'ext/popper.min.js',
