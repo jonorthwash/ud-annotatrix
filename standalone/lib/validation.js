22dd4d2eb9c5055c6a3ccbafbb060bdca7ecaa8e
Francis Tyers <ftyers@prompsit.com>
2017-10-31 16:49:07 +0100

    comments

--- standalone/lib/validation.js
+++ standalone/lib/validation.js
@@ -2,9 +2,14 @@
 
 var U_DEPRELS = ["acl", "advcl", "advmod", "amod", "appos", "aux", "case", "cc", "ccomp", "clf", "compound", "conj", "cop", "csubj", "dep", "det", "discourse", "dislocated", "expl", "fixed", "flat", "goeswith", "iobj", "list", "mark", "nmod", "nsubj", "nummod", "obj", "obl", "orphan", "parataxis", "punct", "reparandum", "root", "vocative", "xcomp"];
 var U_POS = ["ADJ", "ADP", "ADV", "AUX", "CCONJ", "DET", "INTJ", "NOUN", "NUM", "PART", "PRON", "PROPN", "PUNCT", "SCONJ", "SYM", "VERB", "X"];
+
+// TODO: Make this more clever, e.g. CCONJ can have a dependent in certain 
+// circumstances, e.g. and / or 
 var U_POS_LEAF = ["ADP", "AUX", "CCONJ", "PART", "PUNCT", "SCONJ"];
 
 function is_udeprel(s) {
+    // Checks if a relation is in the list of valid relations
+    // @s = the input relation
     var s_deprel = s;
     // Language specific relations are a universal relation + : + some string
     if(s.search(":") >= 0) {
@@ -20,7 +25,9 @@ function is_udeprel(s) {
 }
 
 function is_leaf(s) {
-
+    // Checks if a node is in the list of part-of-speech tags which 
+    // are usually leaf nodes
+    // @s = part of speech tag
     for(var i = 0; i < U_POS_LEAF.length; i++) {
       if(U_POS_LEAF[i] == s) { 
         return true;
@@ -30,7 +37,9 @@ function is_leaf(s) {
 
 }
 
-function is_cyclic(s) {
+function is_cyclic(tree) {
+    // Checks if a given tree is cyclic 
+    // @tree = input tree
 
-   return false;
+    return false;
 }
