2a1396276ba0792c346b27b9eaf45434f5718c59
Kevin Murphy <kmurphy4@swarthmore.edu>
2018-05-12 01:49:24 -0300

    refactor CG2conllu.js

--- standalone/lib/CG2conllu.js
+++ standalone/lib/CG2conllu.js
@@ -1,51 +1,45 @@
 'use strict'
 
 function CG2conllu(CGtext) {
+    log.debug(`called CG2conllu(${CGtext})`);
+
     /* Takes a string in CG, returns a string in conllu. */
 
     // TODO: Check for '<s>' ... '</s>' and if you have matching things treat them
     // as comments with #
 
-    if (ambiguetyPresent(CGtext)) { // to abort conversion if there are ambiguous analyses
+    if (ambiguityPresent(CGtext)) // to abort conversion if there are ambiguous analyses
         return;
-    }
+
     // remove extra spaces before newline before processing text
     CGtext = CGtext.replace(/ +\n/, '\n');
-    var sent = new conllu.Sentence();
-    var comments = findComments(CGtext);
-    sent.comments = comments;
-    var tokens = formTokens(CGtext);
-    sent.tokens = tokens;
+    let sent = new conllu.Sentence();
+    sent.comments = findComments(CGtext);
+    sent.tokens = formTokens(CGtext);
+
     return sent.serial;
 }
 
 
 function findComments(CGtext) {
     /* Takes a string in CG, returns 2 arrays with strings. */
-    var lines = CGtext.split('\n');
-    var comments = [];
-    $.each(lines, function(n, line) {
-        if (line[0] == '#') {
-            line = line.replace(/^#+/, '');
-            comments.push(line);
-        }
-    });
-    return comments;
+    return CGtext.split('\n')
+        .filter((line) => { return line[0] === '#' })  // take only strings beginning with "#"
+        .map((line) => { return line.replace(/^#+/, ''); }); // string off leading "#"s
 }
 
 
-function ambiguetyPresent(CGtext) {
-    var lines = CGtext.split(/"<(.*)>"/);
+function ambiguityPresent(CGtext) {
 
     // suppose the indent is consistent troughout the sentence
-    for (var i = 2; i < lines.length; i += 2) {
-        var indent = lines[i].replace('\n', '').split(/[^\s]/)[0];
-        var ana = lines[i].trim();
-        if (ana.includes(indent) && !ana.includes(indent + indent)) {
-            // console.log(lines[i]);
+    const lines = CGtext.split(/"<(.*)>"/);
+    for (let i = 2, l = lines.length; i < l; i += 2) {
+        const indent = lines[i].replace('\n', '').split(/[^\s]/)[0],
+            ana = lines[i].trim();
+        if (ana.includes(indent) && !ana.includes(indent + indent))
             return true;
-        }
     }
+
     return false;
 }
 
@@ -53,113 +47,123 @@ function ambiguetyPresent(CGtext) {
 function formTokens(CGtext) {
 
     // i use the presupposition that there are no ambiguous readings,
-    // because i've aboted conversion of ambiguous sentences in ambiguetyPresent
-    var tokens = [];
-    var lines = CGtext.split(/"<(.*)>"/).slice(1);
-    var tokId = 1;
-    $.each(lines, function(n, line) {
-        if (n % 2 == 1) {
-            var form = lines[n - 1];
+    // because i've aborted conversion of ambiguous sentences in ambiguityPresent
+    let tokens = [], tokenId = 1;
+    $.each(CGtext.split(/"<(.*)>"/).slice(1), (i, line) => {
+        if (i % 2 === 1) {
+            const form = lines[i - 1];
             line = line.replace(/^\n?;?( +|\t)/, '');
             if (!line.match(/(  |\t)/)) {
-                var token = getAnalyses(line, { form:form, id:tokId});
+                let token = getAnalyses(line, { form:form, id:tokenId });
                 tokens.push(formNewToken(token));
-                tokId ++;
+                tokenId ++;
             } else {
-                var subtokens = line.trim().split('\n');
-                var supertoken = formSupertoken(subtokens, form, tokId);
+                const subtokens = line.trim().split('\n'),
+                    supertoken = formSupertoken(subtokens, form, tokenId);
                 tokens.push(supertoken);
-                tokId += subtokens.length;
+                tokenId += subtokens.length;
             }
         }
-    })
+    });
+
     return tokens;
 }
 
 
 function getAnalyses(line, analyses) {
-    console.log(analyses);
+    log.debug(`called getAnalyses(line: ${line}, analyses: ${JSON.stringify(analyses)})`);
+
     // first replace space (0020) with · for lemmas and forms containing
     // whitespace, so that the parser doesn't get confused.
-    var quoted = line.replace(/.*(".*?").*/, '$1');
-    var forSubst = quoted.replace(/ /g, '·');
-    var gram = line.replace(/".*"/, forSubst);
+    const quoted = line.replace(/.*(".*?").*/, '$1'),
+        forSubst = quoted.replace(/ /g, '·'),
+        gram = line.replace(/".*"/, forSubst)
+            .replace(/[\n\t]+/, '').trim().split(' '); // then split on space and iterate
 
-    gram = gram.replace(/[\n\t]+/, '').trim().split(' '); // then split on space and iterate
-    $.each(gram, function(n, ana) {
-        if (ana.match(/"[^<>]*"/)) {
-            analyses.lemma = ana.replace(/"([^<>]*)"/, '$1');
-        } else if (ana.match(/#[0-9]+->[0-9]+/)) {
-            analyses.head = ana.replace(/#([0-9]+)->([0-9]+)/, '$2').trim();
+    $.each(gram, (i, analysis) => {
+        if (analysis.match(/"[^<>]*"/)) {
+
+            analyses.lemma = analysis.replace(/"([^<>]*)"/, '$1');
+
+        } else if (analysis.match(/#[0-9]+->[0-9]+/)) {
+
             // in CG sometimes heads are the same as the token id, this breaks visualisation #264
-            if(analyses.id == analyses.head) {
+            analyses.head = analysis.replace(/#([0-9]+)->([0-9]+)/, '$2').trim();
+            if (analyses.id === analyses.head)
                 analyses.head = '';
-            }
-        } else if (ana.match(/#[0-9]+->/)) {
+
+        } else if (analysis.match(/#[0-9]+->/)) {
+
             // pass
-        } else if (ana.match(/@[A-Za-z:]+/)) {
-            analyses.deprel = ana.replace(/@([A-Za-z:]+)/, '$1');
-        } else if (n < 2) {
-            analyses.upostag = ana; // TODO: what about xpostag?
-        } else {
-            // saving other data
-            if (!analyses.feats) {
-                analyses.feats = ana;
-            } else {
-                analyses.feats += '|' + ana;
-            }
+
+        } else if (analysis.match(/@[A-Za-z:]+/)) {
+
+            analyses.deprel = analysis.replace(/@([A-Za-z:]+)/, '$1');
+
+        } else if (i < 2) {
+
+            analyses.upostag = analysis; // TODO: what about xpostag?
+
+        } else { // saving other stuff
+
+            analyses.feats = (analyses.feats ? '' : '|') + analysis;
+
         }
-    })
+    });
+
     return analyses;
 }
 
 
 function formNewToken(attrs) {
+    log.debug(`called formNewToken(${JSON.stringify(attrs)})`);
+
     /* Takes a dictionary of attributes. Creates a new token, assigns
     values to the attributes given. Returns the new token. */
 
-    var newToken = new conllu.Token();
-    $.each(attrs, function(attr, val){
+    let newToken = new conllu.Token();
+    $.each(attrs, (attr, val) => {
         newToken[attr] = val;
     });
+
     return newToken;
 }
 
 
-function formSupertoken(subtokens, form, tokId) {
-    var sup = new conllu.MultiwordToken();
+function formSupertoken(subtokens, form, tokenId) {
+    log.debug(`called formSupertoken(subtokens: ${JSON.stringify(subtokens)}, form: ${form}, tokenId: ${tokenId})`);
+
+    let sup = new conllu.MultiwordToken();
     sup.form = form;
 
-    $.each(subtokens, function(n, tok) {
-        var newTok = getAnalyses(tok, { id:tokId, form:'_'});
-        sup.tokens.push(formNewToken(newTok));
-        tokId ++;
-    })
+    $.each(subtokens, (i, token) => {
+        const newToken = getAnalyses(token, { id:tokenId, form:'_' });
+        sup.tokens.push(formNewToken(newToken));
+        tokenId++;
+    });
+
     return sup;
 }
 
 
 function conllu2CG(conlluText, indent) {
-    var sent = new conllu.Sentence();
+    log.debug(`called conllu2CG(conllu: ${conlluText}, indent: ${indent})`);
+
+    let sent = new conllu.Sentence();
     sent.serial = conlluText;
-    if (indent == undefined) {
-        var indent = '\t';
-    }
+    indent = indent || '\t';
 
-    var CGtext = (sent.comments.length) ? '#' + sent.comments.join('\n#') : '';
+    let CGtext = (sent.comments.length ? '#' + sent.comments.join('\n#') : '');
 
-    $.each(sent.tokens, function(i, tok) {
+    $.each(sent.tokens, (i, token) => {
 
-        var form = (tok.form) ? ('\n"<' + tok.form + '>"\n') : '';
-        CGtext += form;
-        if (tok.tokens == undefined) {
-            CGtext += indent + newCgAna(i, tok);
+        CGtext += (token.form ? `\n"<${token.form}>"\n"` : '');
+        if (token.tokens == undefined) {
+            CGtext += indent + newCGAnalysis(i, token);
         } else {
-            var anas = [];
-            $.each(tok.tokens, function(j, subtok) {
-                 anas.push(indent.repeat(j + 1) + newCgAna(j, subtok));
-            })
-            CGtext += anas.join('\n');
+            CGtext += token.tokens.map((subtoken, j) => {
+                return indent.repeat(j+1) + newCGAnalysis(j, subtoken);
+            }).join('\n');
         }
     })
 
@@ -167,13 +171,16 @@ function conllu2CG(conlluText, indent) {
 }
 
 
-function newCgAna(i, tok) {
-    var lemma = (tok.lemma) ? ('"' + tok.lemma + '"') : '""'; // lemma should have "" if blank  #228
-    var pos = (tok.upostag) ? tok.upostag : tok.xpostag;
-    if (pos == undefined) { pos = '_' };
-    var feats = (tok.feats) ? ' ' + tok.feats.replace(/\|/g, ' ') : '';
-    var deprel = (tok.deprel) ? ' @' + tok.deprel : ' @x'; // is it really what we want by default?
-    var head = (tok.head) ? tok.head : '';
-    var cgToken = lemma + ' ' + pos + feats + deprel + ' #' + tok.id + '->' + head;
+function newCGAnalysis(i, token) {
+    log.debug(`called newCGAnalysis(i: ${i}, token: ${token})`);
+
+    const lemma = (token.lemma ? `"${token.lemma}"` : `""`), // lemma should have "" if blank (#228)
+        pos = token.upostag || token.xpostag || '_',
+        feats = (token.feats ? ` ${token.feats.replace(/\|/g, ' ')}` : '');
+        deprel = (token.deprel ? ` @${token.deprel}` : ' @x'); // is it really what we want by default?
+        head = token.head || '';
+        cgToken = `${lemma} ${pos}${feats}${deprel} #${token.id}->${head}`;
+
+    log.debug(`got cgToken: ${cgToken}`);
     return cgToken;
 }
