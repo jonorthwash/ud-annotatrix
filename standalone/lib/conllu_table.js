3c77f23c94029018159d6e547cc24d6a6b676962
Kevin Murphy <kmurphy4@swarthmore.edu>
2018-05-26 20:21:03 -0300

    editing table saves changes to _

--- standalone/lib/conllu_table.js
+++ standalone/lib/conllu_table.js
@@ -4,6 +4,151 @@ var TABLE_COLUMNS_VISIBILITY = new Array(10).fill(true),
     IS_TABLE_VIEW = false,
     DEFAULT_NUM_TABLE_ROWS = 20
 
+
+
+
+
+function toggleTableView(event, force) { // force param used for testing
+    log.debug('called toggleTableView()');
+
+    if (_.formats[_.current] !== 'CoNLL-U') {
+        log.warn(`toggleTableView(): table view not supported for ${_.formats[_.current]}`);
+        _.is_table_view = false;
+    } else {
+        _.is_table_view = (force === undefined ? !_.is_table_view : force);
+    }
+
+    updateTabs();
+}
+
+function onEditTable(event) {
+    log.debug(`called onEditTable(key: ${event.which})`);
+
+    switch (event.which) {
+        case (KEYS.ENTER):
+            onEnter(event);
+            break;
+        default:
+
+            // join the rows on \n and the columns on \t
+            let conllu = Array.from($('#table-data tr').map((i, tr) => {
+                if ($(tr).hasClass('comment') || $(tr).hasClass('wrong-shape')) {
+
+                    return $(tr).text();
+
+                } else {
+
+                    return Array.from($(tr).find('td').map((j, td) => {
+                        let content = $(td).text().replace(/<br>/g, '').trim();
+                        return content.length ? content : '_';
+
+                    })).join('\t');
+                }
+            })).join('\n');
+
+            // save it to the textarea and parse it
+            $('#text-data').val(conllu);
+            parseTextData();
+    }
+}
+
+function buildTable() {
+    log.debug(`called buildTable()`);
+
+    $('#table-data tbody').empty();
+    $.each($('#text-data').val().split('\n'), (i, line) => {
+        log.debug(`updateTable() line: ${line}`);
+        if (line.trim() === '')
+            return
+
+        const cells = line.split('\t');
+        let tr = $('<tr>').attr('id', `table_${i}`);
+
+        if (line.startsWith('#')) {
+
+            tr.addClass('comment').text(line);
+
+        } else if (cells.length !== 10) {
+
+            log.warn(`buildTable(): CoNLL-U should have 10 columns`);
+            tr.addClass('wrong-shape').text(line);
+
+        } else {
+
+            $.each(cells, (j, cell) => {
+                let valid = {},
+                    span = $('<span>');
+
+                if (cell.trim() === '')
+                    cell = '_';
+
+                if (cell !== '_') {
+                    if (j === 3)
+                        valid = is_upos(cell);
+                    if (j === 7)
+                        valid = is_udeprel(cell);
+                }
+
+                span.text(cell)
+                    .attr('id', `table_${i}_${j}`)
+                    .attr('row-id', i)
+                    .attr('col-id', j)
+                    .attr('data-value', cell)
+                    .attr('display', _.column_visible[j] ? 'inline' : 'hidden')
+                    .attr('tabIndex', -1)
+                    .prop('contenteditable', true)
+                    .keyup(onEditTable);
+
+                if (valid.err) {
+                    log.warn(`buildTable(): error parsing cell (err:"${valid.err}", cell:"${cell}")`);
+                    span.addClass('parse-error');
+                    document.l10n.formatValue(valid.err, valid.data).then( (title) => {
+                        span.addClass('fa fa-exclamation-triangle')
+                            .attr('aria-hidden', 'true')
+                            .attr('title', title);
+                    });
+                }
+                tr.append( $('<td>').append(span) );
+            });
+        }
+        $('#table-data tbody').append(tr);
+    });
+}
+
+function toggleTableColumn(event) {
+    log.debug(`called toggleTableColumn(col-id: ${$(event.target).attr('col-id')})`);
+
+    const target = $(event.target), col = target.attr('col-id');
+    if (target.find('i').length === 0) {
+        log.warn(`toggleTableColumn(): `)
+    }
+        return;
+
+    _.column_visible[col] = !_.column_visible[col];
+    target.toggleClass('column-hidden')
+        .find('i').toggleClass('fa-angle-double-right', 'fa-angle-double-left');
+    $(`td [col-id=${col}]`).toggle();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function calculateRows() {
     log.debug('called calculateRows()');
 
@@ -33,153 +178,7 @@ function fitTable() {
 
 
 function tableEditCell(loc) {
-    log.debug(`called tableEditCell(${loc})`);
-
-    loc = loc.trim();
-    const table = $('#table-data'),
-        cell = $(`#${loc}`).html();
-
-    log.debug(`tableEditCell() cell: ${cell}`);
-
-    // Update the CoNLL-U and set the value in the textbox
-    let conllu = '';
-    $.each(table.find('tr'), (i, tr) => {
-        $.each($(tr).find('td'), (j, td) => {
-            let content = $(td).text();
-            if (content.trim() === '')
-                content = '_';
-
-            content = content.replace(/<br>/g, ''); // Get rid of extra spaces
-            conllu += (j > 0 ? '\t' : '') + content;
-            $(td).text(content)
-                .attr('tabindex', '-1')
-                .prop('contenteditable', true)
-                .blur(updateTable)
-                .keyup(() => { tableEditCell(`table_${i}_${j}`)});
-        });
-        conllu += '\n';
-    });
-
-    log.debug(`tableEditCell() conllu: ${conllu}`);
-
-    $('#text-data').val(conllu);
-    drawTree();
 }
-
-
-
-
-
-
-function toggleTableView(event, force) { // force param used for testing
-    log.debug('called toggleTableView()');
-
-    if (_.formats[_.current] !== 'CoNLL-U') {
-        log.warn(`toggleTableView(): table view not supported for ${_.formats[_.current]}`);
-        _.is_table_view = false;
-    } else {
-        _.is_table_view = (force === undefined ? !_.is_table_view : force);
-    }
-
-    updateTabs();
-}
-
-function onEditTable(event) {
-    log.debug(`called onEditTable(key: ${event.which})`);
-
-    throw new NotImplementedError('onEditTable() not implemented');
-
-    switch (event.which) {
-        case (KEYS.ENTER):
-            onEnter(event);
-            break;
-        default:
-            //
-    }
-}
-
-function buildTable() {
-    log.debug(`called buildTable()`);
-
-    $('#table-data tbody').empty();
-    $.each($('#text-data').val().split('\n'), (i, line) => {
-        log.debug(`updateTable() line: ${line}`);
-        if (line.trim() === '')
-            return
-
-        const cells = line.split('\t');
-        let tr = $('<tr>').attr('id', `table_${i}`);
-
-        if (line.startsWith('#')) {
-            tr.addClass('comment').text(line);
-            return;
-        }
-
-        if (cells.length !== 10) {
-            log.warn(`buildTable(): CoNLL-U should have 10 columns`);
-            tr.addClass('wrong-shape').text(line);
-            return;
-        }
-
-        $.each(cells, (j, cell) => {
-            let valid = {},
-                span = $('<span>');
-
-            if (cell.trim() === '')
-                cell = '_';
-
-            if (cell !== '_') {
-                if (j === 3)
-                    valid = is_upos(cell);
-                if (j === 7)
-                    valid = is_udeprel(cell);
-            }
-
-            span.text(cell)
-                .attr('id', `table_${i}_${j}`)
-                .attr('row-id', i)
-                .attr('col-id', j)
-                .attr('data-value', cell)
-                .attr('display', _.column_visible[j] ? 'inline' : 'hidden')
-                .attr('tabIndex', -1)
-                .prop('contenteditable', true)
-                .keyup(onEditTable);
-
-            if (valid.err) {
-                log.warn(`buildTable(): error parsing cell (err:"${valid.err}", cell:"${cell}")`);
-                span.addClass('parse-error');
-                document.l10n.formatValue(valid.err, valid.data).then( (title) => {
-                    span.addClass('fa fa-exclamation-triangle')
-                        .attr('aria-hidden', 'true')
-                        .attr('title', title);
-                });
-            }
-            tr.append( $('<td>').append(span) );
-        });
-        $('#table-data tbody').append(tr);
-    });
-}
-
-function toggleTableColumn(event) {
-    log.critical(`called toggleTableColumn(col-id: ${$(event.target).attr('col-id')})`);
-
-    const target = $(event.target);
-    if (target.find('i').length === 0)
-        return;
-
-    target.toggleClass('column-hidden')
-        .find('i').toggleClass('fa-angle-double-right', 'fa-angle-double-left');
-    $(`td [col-id=${target.attr('col-id')}]`).toggle();
-
-}
-
-
-
-
-
-
-
-
 
 function updateTable() {
     log.debug('called updateTable()');
