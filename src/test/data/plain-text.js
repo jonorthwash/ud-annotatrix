2447b5186e02a85f885ed7b0e59051fc9cb431c6
Kevin Murphy <kmurphy4@swarthmore.edu>
2018-07-11 11:05:18 -0400

    add data from issue #292

--- src/test/data/plain-text.js
+++ src/test/data/plain-text.js
@@ -6,5 +6,6 @@ module.exports = {
 2: 'this is a test...',
 3: 'this is a test?',
 4: '\tthis is a test',
-5: 'More sentences = more data; ipso facto, yes.'
+5: 'More sentences = more data; ipso facto, yes.',
+parens_and_numbers: `Дәүләтләрнең, шул исәптән Русиянең дә, диңгез чикләре ярдан 12 миль (яки 22,2 км) ераклыкта узуы килешенгән`
 };
