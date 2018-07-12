fb314f420178daf089b4d560ff2e69f81a6f8976
Kevin Murphy <kmurphy4@swarthmore.edu>
2018-07-12 11:14:01 -0400

    add tests for picking a text color for 2 possible background colors

--- src/test/labels.js
+++ src/test/labels.js
@@ -13,37 +13,67 @@ const LabelManager = require('../labels');
 const errors = require('../errors');
 
 module.exports = () => {
-  describe('parse comments individually', () => {
+  describe('labels.js', () => {
+    describe('parse comments individually', () => {
 
-    const _data = [
-      {
-        name: 'labels_1',
-        labels: ['label1', 'another_label', 'a-third-label']
-      },
-      {
-        name: 'labels_2',
-        labels: ['one_label', 'second', 'third-label',
-          'row_2', 'again:here', 'this', 'that' ]
-      },
-      {
-        name: 'labels_3',
-        labels: ['this-is-a-tag', 'test', 'testing']
-      },
-      {
-        name: 'nested_2',
-        labels: []
-      }
-    ];
+      const _data = [
+        {
+          name: 'labels_1',
+          labels: ['label1', 'another_label', 'a-third-label']
+        },
+        {
+          name: 'labels_2',
+          labels: ['one_label', 'second', 'third-label',
+            'row_2', 'again:here', 'this', 'that' ]
+        },
+        {
+          name: 'labels_3',
+          labels: ['this-is-a-tag', 'test', 'testing']
+        },
+        {
+          name: 'nested_2',
+          labels: []
+        }
+      ];
 
-    _.each(_data, datum => {
-      it(`should parse labels from CoNLL-U:${datum.name}`, () => {
+      _.each(_data, datum => {
+        it(`should parse labels from CoNLL-U:${datum.name}`, () => {
 
-        const conllu = data['CoNLL-U'][datum.name],
-          s = nx.Sentence.fromConllu(conllu),
-          labeler = new LabelManager();
+          const conllu = data['CoNLL-U'][datum.name],
+            s = nx.Sentence.fromConllu(conllu),
+            labeler = new LabelManager();
 
-        labeler.parse(s.comments);
-        expect(labeler.labels.map(label => label.name)).to.deep.equal(datum.labels);
+          labeler.parse(s.comments);
+          expect(labeler.labels.map(label => label.name)).to.deep.equal(datum.labels);
+        });
+      });
+    });
+
+    describe('parse comments on aggregate', () => {
+
+    });
+
+    describe('pick a text color sanely', () => {
+
+      const labelName = 'test',
+        label = new LabelManager().add(labelName).get(labelName);
+
+      const _data = [
+        {
+          bColor: '#000000',
+          tColor: '#ffffff'
+        },
+        {
+          bColor: '#ffffff',
+          tColor: '#000000'
+        }
+      ];
+
+      _.each(_data, datum => {
+        it(`should pick correctly when background="${datum.bColor}"`, () => {
+          label.changeColor(datum.bColor);
+          expect(label.tColor).to.equal(datum.tColor);
+        });
       });
     });
   });
