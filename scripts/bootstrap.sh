5d2491930dc58f010bbab686d8f01530a14fcd3b
Kevin Murphy <kmurphy4@swarthmore.edu>
2018-07-15 15:00:06 -0400

    some more bootstrap tweaks

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,9 +3,7 @@
 # colors
 GREEN="\033[1;32m"
 RESET="\033[0m"
-
-alias echo="echo -e"
-alias runserver="python3 server/server.py"
+RED="\033[1;91m"
 
 # python virtual environment setup
 VENV=""
@@ -13,25 +11,29 @@ if hash virtualenv 2>/dev/null; then
   VENV="ud-env"
 
   if [ ! -d $VENV ]; then
-    virtualenv --no-site-packages $VENV --python=python3.6
+    virtualenv $VENV --python=python3.6
   fi
   . ./$VENV/bin/activate
 
   # virtual environment use instructions
   echo ""
-  echo "NOTE: Python virtual environment activated ($GREEN$VENV$RESET);"
-  echo "  to deactivate, type ${GREEN}deactivate${RESET}."
+  echo -e "NOTE: Python virtual environment activated ($GREEN$VENV$RESET);"
+  echo -e "  to deactivate, type ${GREEN}deactivate${RESET}."
   echo ""
 
+else
+  echo -e "NOTE: ${RED}command${RESET} virtualenv ${RED}not found${RESET}."
 fi
 
 # install required python packages
 # without "Requirement already satisfied warnings"
+echo "installing python packages ..." >&2
 pip3 install --user -r requirements.txt 1> >(grep -v 'Requirement already satisfied' 1>&2)
 
 # basic ENV file
 ENV=.env
 if [ ! -f $ENV ]; then
+  echo "writing env file ..." >&2
   echo "VIRTUAL_ENV=$VENV
 PATH_TO_CORPORA=corpora
 SECRET_KEY=annotatrixareforkids
@@ -41,4 +43,5 @@ DEBUG=DEBUG" > $ENV
 fi
 
 # install js stuff
+echo "installing node packages ..." >&2
 npm install
