38ff67df2e53c1d4c097c20bd31ad604489f7124
Kevin Murphy <kmurphy4@swarthmore.edu>
2018-07-21 20:22:52 -0400

    add a link helper-function, stop getTreebankId from failing if we have another url param

--- src/funcs.js
+++ src/funcs.js
@@ -42,14 +42,21 @@ module.exports = {
   },
 
   getTreebankId: () => {
-    const match = location.href.match(/treebank_id=([0-9a-f-]{36})(#|\/|$)/);
-    console.log(match);
+    const match = location.href.match(/treebank_id=([0-9a-f-]{36})(#|\/|$|&)/);
     if (!match) {
       status.error('invalid treebank url, must be valid UUID4');
       throw new Error('invalid treebank url, must be a valid UUID4');
     }
 
     return match[1];
+  },
+
+  link: (href, target='_blank') => {
+    const link = $('<a>')
+      .attr('href', href)
+      .attr('target', target);
+    $('body').append(link);
+    link[0].click();
   }
 
 };
