cf02963344409db17be7b82cad7993792fcaca9a
Kevin Murphy <kmurphy4@swarthmore.edu>
2018-07-21 23:42:09 -0400

    add the users database to the config

--- server/config.js
+++ server/config.js
@@ -6,48 +6,48 @@ const crypto = require('crypto');
 
 require('dotenv').config();
 
-const port = process.env.ANNOTATRIX_PORT
+let cfg = {};
+
+// basic app config
+cfg.port = process.env.ANNOTATRIX_PORT
   || process.env.PORT
   || 5316;
-const host = process.env.ANNOTATRIX_HOST
+cfg.host = process.env.ANNOTATRIX_HOST
   || process.env.HOST
   || 'localhost';
-const protocol = process.env.ANNOTATRIX_PROTOCOL
+cfg.protocol = process.env.ANNOTATRIX_PROTOCOL
   || process.env.PROTOCOL
   || 'http';
-const corpora_path = process.env.ANNOTATRIX_CORPORA_PATH
+cfg.corpora_path = process.env.ANNOTATRIX_CORPORA_PATH
   || process.env.CORPORA_PATH
   || process.env.PATH_TO_CORPORA
   || 'corpora';
-const secret = process.env.ANNOTATRIX_SECRET
+cfg.secret = process.env.ANNOTATRIX_SECRET
   || process.env.SECRET
   || 'dev secret';
-const environment = process.env.ANNOTATRIX_ENV
+cfg.environment = process.env.ANNOTATRIX_ENV
   || process.env.NODE_ENV
   || 'development';
 
-let github = {
+// oauth config
+cfg.github = {
   client_id: process.env.ANNOTATRIX_GH_CLIENT_ID
     || process.env.GH_CLIENT_ID
     || '298b7a22eb8bc53567d1', // keggsmurph21 'UD-Annotatrix test 2'
   client_secret: process.env.ANNOTATRIX_GH_CLIENT_SECRET
     || process.env.GH_CLIENT_SECRET,
-  login_uri: `${protocol}://${host}:${port}/oauth/login`,
-  callback_uri: `${protocol}://${host}:${port}/oauth/callback`,
+  login_uri: `${cfg.protocol}://${cfg.host}:${cfg.port}/oauth/login`,
+  callback_uri: `${cfg.protocol}://${cfg.host}:${cfg.port}/oauth/callback`,
   state: crypto.randomBytes(8).toString('hex')
 };
 
-if (!github.client_secret)
+if (!cfg.github.client_secret)
   throw new errors.ConfigError('Please provide ANNOTATRIX_GH_CLIENT_SECRET');
 
-mkdirp(corpora_path);
+// database config
+mkdirp(cfg.corpora_path);
+cfg.users_db_path = process.env.ANNOTATRIX_USERS_DB_PATH
+  || '.users';
+cfg.users = require('./db/users')(cfg.users_db_path);
 
-module.exports = {
-  port,
-  host,
-  protocol,
-  corpora_path,
-  secret,
-  environment,
-  github
-};
+module.exports = cfg;
