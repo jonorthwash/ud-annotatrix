b9cdfb05502ebe6ebe494bfabfbfddbb828f31f7
Kevin Murphy <kmurphy4@swarthmore.edu>
2018-07-23 20:52:43 -0400

    add more defaults into new meta tables

--- server/models/corpus-init.js
+++ server/models/corpus-init.js
@@ -16,8 +16,42 @@ CREATE TABLE meta (
   permissions,
   editors
 );
-INSERT INTO meta (current_index, permissions, editors) VALUES (
+INSERT INTO meta (current_index, gui, labeler, permissions, editors) VALUES (
   -1,
+  '{
+    "menu": {
+      "is_visible": true,
+      "pinned": {
+        "login": false,
+        "manage-permissions": false,
+				"save-corpus": false,
+				"upload-corpus": false,
+				"download-corpus": false,
+				"discard-corpus": false,
+				"export-as-latex": false,
+				"export-as-png": false,
+				"export-as-svg": false,
+				"show-help": true,
+				"show-settings": false,
+				"show-table": false
+      }
+    },
+    "is_textarea_visible": true,
+		"are_labels_visible": true,
+		"is_vertical": false,
+		"is_ltr": true,
+		"is_enhanced": false,
+		"readonly": false,
+		"pan": {
+      "x": 0,
+      "y": 0
+    },
+    "zoom": 1
+  }',
+  '{
+    "labels": [],
+    "filter": []
+  }'
   '{
     "allow": null,
     "disallow": [],
