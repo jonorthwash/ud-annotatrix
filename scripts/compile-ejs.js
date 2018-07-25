7c8099d6dfcbc97f8890301a1ee0d4e0558b1d69
Kevin Murphy <kmurphy4@swarthmore.edu>
2018-07-25 16:53:23 -0400

    added a script to compile the EJS down to HTML so that we can use the standalone version

---
+++ scripts/compile-ejs.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const ejs = require('ejs');
+const fs = require('fs');
+const path = require('path');
+
+const mkdirp = require('mkdirp');
+const html_base_path = path.join('server', 'public', 'html');
+mkdirp(html_base_path);
+
+function render(filename, args) {
+  const ejs_path = path.join('server', 'views', `${filename}.ejs`);
+  const html_path = path.join('server', 'public', 'html', `${filename}.html`);
+
+  fs.readFile(ejs_path, (err, contents) => {
+    if (err)
+      throw err;
+
+    contents = contents.toString();
+    const html = ejs.render(contents, args);
+
+    fs.writeFile(html_path, html, err => {
+      if (err)
+        throw err;
+    });
+  });
+}
+
+render('index', {
+  base: null,
+  error: null,
+  treebanks: []
+});
+render('annotatrix', {
+  username: null
+});
+render('help', {});
