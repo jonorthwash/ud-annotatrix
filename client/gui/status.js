202580b58b75abbf6808b8115762d46d829c3a35
Kevin Murphy <kmurphy4@swarthmore.edu>
2018-08-15 18:47:22 -0400

    add autoparsing on/off support

--- client/gui/status.js
+++ client/gui/status.js
@@ -17,33 +17,34 @@ class Status {
     // turn off autoparsing
     $('#parse-status').click(e => {
 
-      const corpus = self.gui.app.corpus;
+      const gui = self.gui;
+      gui.config.autoparsing = !gui.config.autoparsing;
 
-      if (corpus.parsed) {
-
-        corpus.unparsed = $('#text-data').val();
-        self.gui.refresh();
-
+      if (gui.config.autoparsing) {
+        self.gui.app.corpus.parse($('#text-data').val());
       } else {
-
-        corpus.parse(corpus.unparsed);
-
+        self.gui.app.corpus.current.input = $('#text-data').val();
+        self.gui.app.corpus.format = null;
       }
+
+      gui.save();
+      gui.refresh();
     });
   }
 
   refresh() {
 
     const corpus = this.gui.app.corpus,
-      graph = this.gui.app.graph;
+      graph = this.gui.app.graph,
+      gui = this.gui;
 
     $('#parse-status')
       .removeClass('red green')
-      .toggleClass(corpus.parsed ? 'green' : 'red')
-      .text(corpus.parsed ? 'auto' : 'off');
+      .addClass(gui.config.autoparsing ? 'green' : 'red')
+      .text(gui.config.autoparsing ? 'on' : 'off');
 
     let graphStatus;
-    if (!corpus.parsed) {
+    if (!corpus.isParsed) {
 
       graphStatus = 'blocked';
 
@@ -75,8 +76,7 @@ class Status {
 
     $('#graph-status')
       .removeClass('red green')
-      .toggleClass('red', !corpus.parsed)
-      .toggleClass('green', corpus.parsed)
+      .addClass(corpus.isParsed ? 'green' : 'red')
       .text(graphStatus);
 
   }
