fa7ae285714e8410c1566d9f56d5b90ba80268a9
Kevin Murphy <kmurphy4@swarthmore.edu>
2018-08-15 12:37:08 -0400

    cleanup and document

--- client/graph/sort.js
+++ client/graph/sort.js
@@ -5,6 +5,12 @@ const _ = require('underscore');
 // NB: sorting will break if sentence has more than this many tokens
 const LARGE_NUMBER = 10000;
 
+/**
+ * Function to sort nodes with vertical orientation.
+ *
+ * @param {CytoscapeNode} n1
+ * @param {CytoscapeNode} n2
+ */
 function vertical(n1, n2) {
 
   const num = (node) => {
@@ -25,6 +31,12 @@ function vertical(n1, n2) {
       :  1);
 }
 
+/**
+ * Function to sort nodes with left-to-right orientation.
+ *
+ * @param {CytoscapeNode} n1
+ * @param {CytoscapeNode} n2
+ */
 function ltr(n1, n2) {
 
   const num = (node) => {
@@ -45,7 +57,14 @@ function ltr(n1, n2) {
       :  1);
 }
 
+/**
+ * Function to sort nodes with right-to-left orientation.
+ *
+ * @param {CytoscapeNode} n1
+ * @param {CytoscapeNode} n2
+ */
 function rtl(n1, n2) {
+  
   const num = (node) => {
     const id = parseInt(node.data('num')),
       offset = node.data('name') === 'pos-node'
