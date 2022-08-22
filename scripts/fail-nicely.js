322ca2b799130bb2e57eb29ea85236c4767abf04
Kevin Murphy <keggsmurph21@gmail.com>
2022-08-21 20:55:25 -0400

    scripts: Remove the `fail-nicely.*` scripts

--- scripts/fail-nicely.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-
-console.log("Fatal server error:\n" +
-            "There is probably some helpful output above. Try running \"npm install\"\n" +
-            "  before trying again.\n\n" +
-            "For more info, check the README or file an issue at\n" +
-            "  https://github.com/jonorthwash/ud-annotatrix/issues");
